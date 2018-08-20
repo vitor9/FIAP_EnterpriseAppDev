d7e59c4970a38d0f404f31429ad5118eb2e507de
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-20 20:52:29 -0300

    Criado ASP.NET MVC Razor

---
+++ Aula05/05.Fiap.Web.MVC/05.Fiap.Web.MVC/Controllers/OficinaController.cs
@@ -0,0 +1,42 @@
+﻿using _05.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _05.Fiap.Web.MVC.Controllers
+{
+    public class OficinaController : Controller
+    {
+
+        private static List<Oficina> _lista = new List<Oficina>();
+
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_lista);
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            List<string> tipos = new List<string>();
+            tipos.Add("Elétrica");
+            tipos.Add("Mecanica");
+            tipos.Add("Funilaria");
+            ViewBag.listaTipo = new SelectList(tipos);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Oficina oficina)
+        {
+            _lista.Add(oficina);
+            TempData["msg"] = "Oficina cadastrada com sucesso!";
+            return RedirectToAction("Cadastrar");
+        }
+
+    }
+}
